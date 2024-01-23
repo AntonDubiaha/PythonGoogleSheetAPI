7b097066641405de7a8e39daf44fe3c3cd589a0a
AntonDubiaha <anton.dubyaga@gmail.com>
2024-01-23 15:09:59 +0200

    Update parser

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Guide on Setting Up Google API https://youtu.be/3wC-SCdJK2c?si=LAnge3P9hJWu8Vmu
 
 Google Developers Console https://console.cloud.google.com/cloud-resource-manager?pli=1
 
-Requirements: pip install selenium webdriver_manager httplib2 oauth2client
+Requirements: pip install google-api-python-client httplib2 oauth2client selenium webdriver_manager
 
-File "readfromsheet.py" retrieves values from the table and writes them to the "id_products.txt" file. File "parser.py" parses the website using the values from the "id_products.txt" file. File "writetosheet.py" writes values from the "charvalues.txt" file to the table.
+File "readfromsheet.py" retrieves values from the table and writes them to the "id_products.txt" file. 
+File "parser.py" parses the website using the values from the "id_products.txt" file. 
+File "writetosheet.py" writes values from the "charvalues.txt" file to the table.
--- myparser.py
+++ myparser.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -10,6 +13,9 @@ driver = webdriver.Chrome(service=service, options=options)
 
 file_name = "id_products.txt"
 output_file_name = "charvalues.txt"
+
+# Text for xpath
+xpath_text = "Додатково"
 
 with open(output_file_name, "w", encoding="utf-8") as charvalue_file:
     with open(file_name, "r") as file:
@@ -24,12 +30,31 @@ with open(output_file_name, "w", encoding="utf-8") as charvalue_file:
 
             driver.get(url)
 
-            driver.find_element(
-                "xpath", "//a[@href='#tab-specification']").click()
+            try:
+                wait = WebDriverWait(driver, 10)
+                element = wait.until(EC.presence_of_element_located(("xpath", "//a[@href='#tab-specification']")))
 
-            value = driver.find_element(
-                "xpath", "//td[contains(text(), 'Матеріал топки')]/following-sibling::td")
-            value = value.text
+                # Attempt to click on the specified element
+                element.click()
+
+                # Dynamically update XPath with the search text
+                dynamic_xpath = f"//td[contains(text(), '{xpath_text}')]/following-sibling::td"
+                value = driver.find_element("xpath", dynamic_xpath).text
+
+            except TimeoutException:
+                # Handle the case where the element is not found within the specified time (timeout)
+                value = "Error with click"
+
+            except Exception as e:
+                # Handle the case where the element is not found
+                if "no such element" in str(e).lower():
+                    if "href='#tab-specification'" in str(e).lower():
+                        value = "Error with click"
+                    elif xpath_text in str(e):
+                        value = "Element is absent"
+                else:
+                    # Handle other exceptions if needed
+                    raise RuntimeError("Unknown error")
 
             # Write the value to the file
             charvalue_file.write(f"{value}\n")
